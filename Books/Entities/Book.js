6ad2a711420c12759472aeca237e5e40c44a6766
ev_geniy <geka_nkz@mail.ru>
2023-05-09 21:59:01 +0700

    011-mongo

--- Books/Entities/Book.js
+++ Books/Entities/Book.js
@@ -1,51 +1,34 @@
-class Book {
-    #id
-    #title
-    #description
-    #authors
-    #favorite
-    #fileCover
-    #fileName
+const { model, Schema } = require("mongoose");
 
-    constructor(book) {
-        this._id = book.id;
-        this._title = book.title;
-        this._description = book.description;
-        this._authors = book.authors;
-        this._favorite = book.favorite;
-        this._fileCover = book.fileCover;
-        this._fileName = book.fileName;
-    }
+const bookSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		default: "",
+	},
+	authors: {
+		type: String,
+		default: "",
+	},
+	favorite: {
+		type: String,
+		default: "",
+	},
+	fileCover: {
+		type: String,
+		default: "",
+	},
+	fileName: {
+		type: String,
+		default: "",
+	},
+	fileBook: {
+		type: String,
+		default: "",
+	},
+});
 
-    get id() {
-        return this.#id;
-    }
-
-    get title() {
-        return this.#title;
-    }
-
-    get description() {
-        return this.#description;
-    }
-
-    get authors() {
-        return this.#authors;
-    }
-
-    get favorite() {
-        return this.#favorite;
-    }
-
-    get fileCover() {
-        return this.#fileCover;
-    }
-
-    get fileName() {
-        return this.#fileName;
-    }
-}
-
-module.exports = {
-    Book
-}
+module.exports = model("Book", bookSchema);
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const {routes_web} = require("./routes_web");
 const {routes_api} = require("./routes_api");
 
@@ -9,10 +11,27 @@ app.use('/public', express.static(__dirname + '/public'))
 routes_web(app);
 routes_api(app);
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
 
-console.log('Server is listening on port ' + (process.env.PORT || 8080));
+app.listen(port);
 
-module.exports = {
-    app: app
+console.log('Server is listening on port ' + (port));
+
+const dbName = process.env.DB_NAME || "mongo";
+
+async function start() {
+    try {
+        const UrlDb = `mongodb://mongo:27017/${dbName}`;
+        await mongoose.connect(UrlDb);
+
+        app.listen(port, () => {
+            console.log(`Server started`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
+
+start();
+
+
